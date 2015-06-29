9cc2b77e554c31d4777bb8df23699a256f620304
Jonathan Niesen <jon.niesen@gmail.com>
2015-06-29 08:41:32 -0400

    Removing a failing and redundant test.

--- spec/defines/rsyslog_imfile_logfile_spec.rb
+++ spec/defines/rsyslog_imfile_logfile_spec.rb
@@ -5,5 +5,4 @@ describe 'rsyslog_imfile::logfile', :type => :define do
   let(:params) { {:filepath => '', :severity => ''} }
 
   it { should contain_exec('restart_rsyslogd').with_command('service rsyslog restart') }
-  it { should contain_file('/etc/rsyslog.d/some_log.conf').with_content('foo') }
 end
